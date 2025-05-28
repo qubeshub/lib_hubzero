a82a35245991655e0058ffbd8951f19bd5c01f4c
M. Drew LaMar <mdlama@wm.edu>
2025-05-28 14:19:50 -0400

    [hubzero_plugin] Squashed commit e7c7113f67...3415c32110
    commit eb3450df72fa9a96dc5b247936d661c7a43016e7
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Wed May 28 14:17:49 2025 -0400
    
        fix: Remove unnecessary reference
    
        (cherry picked from commit e7c7113f67cd936c5d4bb31535817648266b0441)
    
    commit f39be0c091b894e5cfb3d40ce2af80acd21aed9c
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Wed May 28 14:17:16 2025 -0400
    
        fix: Define properties
    
        Refs: https://wiki.php.net/rfc/deprecate_dynamic_properties
        (cherry picked from commit 4d696ad483054f86432326320eac2de0d8452d1b)
    
    commit e42a77d6044a6977ac8e3ea198abbdd50d987f3b
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Wed May 28 14:16:10 2025 -0400
    
        [PHP82] Dynamic Properties are deprecated
    
        (cherry picked from commit 3415c32110f1876a33450d5a8643eebe1f47d0f5)

--- Plugin/Plugin.php
+++ Plugin/Plugin.php
@@ -59,11 +59,26 @@ class Plugin extends Obj
 	protected $_type = null;
 
 	/**
+	 * Component this plugin might be associated with
+	 * @TODO: Remove this
+	 *
+	 * @var   string
+	 */
+	public $option = null;
+
+	/**
 	 * Affects constructor behavior. If true, language files will be loaded automatically.
 	 *
 	 * @var  boolean
 	 */
 	protected $_autoloadLanguage = false;
+
+	/**
+	 * The triggered event
+	 *
+	 * @var object
+	 */
+	public $event;
 
 	/**
 	 * Constructor
